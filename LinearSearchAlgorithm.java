ce889fc91d9e94fa3092240efa16e50b9a64912b
harsh200603 <harshgupta.ncer@gmail.com>
2024-07-23 18:57:47 +0530

    code added

--- LinearSearchAlgorithm.java
+++ LinearSearchAlgorithm.java
@@ -1,4 +1,25 @@
 package ArrayPrograms;
-
+import java.util.*;
 public class LinearSearchAlgorithm {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int [] arr={12,34,65,73,89,96,-2,30,-91};
+        System.out.println("Array : "+Arrays.toString(arr));
+        System.out.println("Enter the element to found");
+        int target=sc.nextInt();
+        System.out.println("The element is present at index :" + LinearSearch(arr,target));
+
+    }
+    static int LinearSearch(int [] arr , int target)
+    {
+        if(arr.length==0)
+            return -1;
+        for(int index=0;index<arr.length;index++)
+        {
+            int element=arr[index];
+            if(element==target)
+                return index;
+        }
+        return 0;
+    }
 }
