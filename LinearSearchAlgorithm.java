1f0fe0a1057741b309ff08ce23318200f55cfd97
harsh200603 <harshgupta.ncer@gmail.com>
2024-07-31 12:42:23 +0530

    Updating Comments

--- LinearSearchAlgorithm.java
+++ LinearSearchAlgorithm.java
@@ -2,23 +2,28 @@ package ArrayPrograms;
 import java.util.*;
 public class LinearSearchAlgorithm {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        //Initializing and Printing the array.
         int [] arr={12,34,65,73,89,96,-2,30,-91};
         System.out.println("Array : "+Arrays.toString(arr));
+
+        //Input of the target element.
+        Scanner sc=new Scanner(System.in);
         System.out.println("Enter the element to found");
         int target=sc.nextInt();
+
+        //Calling the function to find the target element in array.
         System.out.println("The element is present at index :" + LinearSearch(arr,target));
 
     }
     static int LinearSearch(int [] arr , int target)
     {
-        if(arr.length==0)
+        if(arr.length==0) //checks if the array contains some elements or it is an empty array.
             return -1;
-        for(int index=0;index<arr.length;index++)
+        for(int index=0;index<arr.length;index++)//Iterates over the array
         {
-            int element=arr[index];
+            int element=arr[index]; // Initially arr[index] will be 0 so element will be zero
             if(element==target)
-                return index;
+                return index;  //returns index no if element equals the target element.
         }
         return -1;
     }
--- Search_in_2dArray.java
+++ Search_in_2dArray.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 
 public class Search_in_2dArray {
     public static void main(String[] args) {
+        //Initializing 2D Array
         int [][] arr={
                 {24,4,1},
                 {18,12,5,9},
                 {118,2}
         };
+
+        //Printing 2D Array
         for(int i=0;i<arr.length;i++)
         {
             for(int j=0; j<arr[i].length;j++)
@@ -18,19 +21,22 @@ public class Search_in_2dArray {
             }
             System.out.println();
         }
+
+        //Input of target to find in array
         Scanner sc=new Scanner(System.in);
         System.out.println("\nEnter the target number to find :");
         int target=sc.nextInt();
 
-        int [] ans=SearchIn2DArray(target , arr);
+        //Calling the function().
+        int [] ans=SearchIn2DArray(target , arr); //ans will be in format showing [row,col] of any particular element
         System.out.println(Arrays.toString(ans));
     }
 
     static int[] SearchIn2DArray(int target,int[][] arr) {
-        for (int row = 0; row < arr.length; row++) {
-            for (int col = 0; col < arr[row].length; col++) {
+        for (int row = 0; row < arr.length; row++) { //iterates through row
+            for (int col = 0; col < arr[row].length; col++) {//iterates through column
                 if (arr[row][col] == target)
-                    return new int[]{row, col};
+                    return new int[]{row, col};//returns row and column as output for the searched element
 
             }
         }
